00aa05b0f3e1f4dcaa4c296d31e4d5be25987476
48459 - Diogo Santos <a48459@alunos.isel.pt>
2024-04-01 22:58:37 +0100

    Added missing Content from file UserController.cs

--- src/market-tracker-webapi/Application/Http/Controllers/UserController.cs
+++ src/market-tracker-webapi/Application/Http/Controllers/UserController.cs
@@ -1,1 +1,116 @@
-﻿
+﻿using market_tracker_webapi.Application.Http.Models;
+using market_tracker_webapi.Application.Http.Models.User;
+using market_tracker_webapi.Application.Http.Problem;
+using market_tracker_webapi.Application.Service.Errors.User;
+using market_tracker_webapi.Application.Service.Operations.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace market_tracker_webapi.Application.Http.Controllers
+{
+    [ApiController]
+    [Route(Uris.Users.Base)]
+    public class UserController(IUserService userService, ILogger<UserController> logger)
+        : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<UsersOutputModel>> GetUsersAsync(
+            [FromQuery] Pagination pagination,
+            [FromQuery] string? username
+        )
+        {
+            logger.LogDebug($"Call {nameof(GetUsersAsync)} with {username}");
+            
+            return Ok(await userService.GetUsersAsync(username, pagination));
+        }
+
+
+        [HttpGet(Uris.Users.UserById)]
+        public async Task<ActionResult<UserOutputModel>> GetUserAsync(Guid id)
+        {
+            logger.LogDebug($"Call {nameof(GetUserAsync)} with {id}");
+
+            return ResultHandler.Handle(
+                await userService.GetUserAsync(id),
+                error =>
+                {
+                    return error switch
+                    {
+                        UserFetchingError.UserByIdNotFound idNotFoundError
+                            => new UserProblem.UserByIdNotFound(idNotFoundError).ToActionResult()
+                    };
+                }
+            );
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserCreationOutputModel>> CreateUserAsync(
+            [FromQuery] int? code,
+            [FromBody] UserCreationInputModel userInput
+        )
+        {
+            logger.LogDebug(
+                $"Call {nameof(CreateUserAsync)} with {userInput.Username}, {userInput.Name}, {userInput.Email}, {userInput.Password}");
+
+            var res = await userService.CreateUserAsync(userInput.Username, userInput.Name, userInput.Email,
+                userInput.Password, code);
+
+            return ResultHandler.Handle(
+                res,
+                error =>
+                {
+                    return error switch
+                    {
+                        UserCreationError.EmailAlreadyInUse emailAlreadyInUse => new UserProblem.UserAlreadyExists(
+                            emailAlreadyInUse).ToActionResult(),
+
+                        UserCreationError.InvalidEmail invalidEmail => new UserProblem.InvalidEmail(invalidEmail)
+                            .ToActionResult()
+                    };
+                },
+                idOutputModel => Created(Uris.Users.BuildUserByIdUri(idOutputModel.Id), idOutputModel)
+            );
+        }
+
+        [HttpPut(Uris.Users.UserById)]
+        public async Task<ActionResult<UserOutputModel>> UpdateUserAsync(
+            Guid id,
+            [FromBody] UserUpdateInputModel userInput
+        )
+        {
+            logger.LogDebug($"Call {nameof(GetUserAsync)} with ");
+
+            return ResultHandler.Handle(
+                await userService.UpdateUserAsync(id, userInput.Name, userInput.Username),
+                error =>
+                {
+                    return error switch
+                    {
+                        UserFetchingError.UserByIdNotFound userByIdNotFound => new UserProblem
+                            .UserByIdNotFound(userByIdNotFound).ToActionResult()
+                    };
+                }
+            );
+        }
+
+        [HttpDelete(Uris.Users.UserById)]
+        public async Task<ActionResult<UserOutputModel>> DeleteUserAsync(
+            Guid id
+        )
+        {
+            logger.LogDebug($"Call {nameof(DeleteUserAsync)} with {id}");
+
+            return ResultHandler.Handle(
+                await userService.DeleteUserAsync(id),
+                error =>
+                {
+                    return error switch
+                    {
+                        UserFetchingError.UserByIdNotFound userByIdNotFound => new UserProblem.UserByIdNotFound(
+                            userByIdNotFound).ToActionResult(),
+                    };
+                },
+                _ => NoContent()
+            );
+        }
+    }
+}
